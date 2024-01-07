e99b8c639f6c986ac9c816867694c9b425a3fe4d
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-01-07 04:57:01 +0300

    feat: solve generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -366,8 +366,9 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  if (len === 0) return [];
+  return new Array(len).fill(1).map((_, index) => 1 + 2 * index);
 }
 
 /**
