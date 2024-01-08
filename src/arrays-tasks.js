99f99c7bcb5cfdfea1fc95168d6c1066f8abf9e6
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-01-08 15:51:09 +0300

    feat: solve getFalsyValuesCount

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -392,8 +392,7 @@ function getElementByIndices(arr, indices) {
     return arr;
   }
 
-  const index = indices.shift();
-  return getElementByIndices(arr[index], indices);
+  return getElementByIndices(arr[indices.shift()], indices);
 }
 
 /**
@@ -408,8 +407,8 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((x) => !x).length;
 }
 
 /**
